8e58b1d59d26f2d6ca26c7da4882ca3d95cde664
valuntiny <459201296@qq.com>
2018-11-14 23:04:39 -0500

    we need bracket for bits operation

---
+++ reverseBits.py
@@ -0,0 +1,14 @@
+'''
+Reverse bits of a given 32 bits unsigned integer.
+'''
+
+class Solution:
+    # @param n, an integer
+    # @return an integer
+    def reverseBits(self, n):
+        res = 0
+        for _ in range(32):
+            res = (res << 1) + (n & 1)
+            n = (n >> 1)
+
+        return res
